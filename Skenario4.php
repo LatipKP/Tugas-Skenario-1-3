fb9b20486e8ba72a7c42f21a4d6d03e48f67123e
Latif Kurnia Prakoso <latifkurniaprakoso@gmail.com>
2025-06-11 18:47:11 +0700

    Add files via upload

---
+++ Skenario4.php
@@ -0,0 +1,16 @@
+<?php
+//kasus 4
+$musik = [
+    "galau" => "Mesin Waktu - Budi Doremi",
+    "bersemangat" => "Selamat Pagi - Ran",
+    "marah" => "Yang Patah Tumbuh, yang Hilang Berganti - Banda Neira"
+];
+
+$suasana = "marah";
+
+if (array_key_exists($suasana, $musik)) {
+    echo "Ambyar sedang $suasana, Ambyar mendengarkan lagu " . $musik[$suasana] . ".";
+} else {
+    echo "Suasana hati tidak dikenali. Tidak ada lagu yang diputar.";
+}
+?>
